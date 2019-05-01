9fafa8907bbe055adc86f2401cd5c00a6bf32ce1
Sai Gokula Krishnan <sendittogokul@gmail.com>
2019-05-01 13:12:55 +0530

    Update snap.sh

--- snap.sh
+++ snap.sh
@@ -1,2 +1,3 @@
 let i=`find . -type f | wc -l`/2 ; find . -type f -print0 | shuf -z -n $i | xargs -0 -- cat 
+## This command just list out the 50% random files in your disk because of the Concatenate command, This is made because for the obvious testing reasons. 
 ## If you want to make delete, what you need to do is turn 'cat' into 'rm'.
